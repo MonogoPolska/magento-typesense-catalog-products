ef736e8a7c15dbc590e367fbe0785ffb48ed935b
Pawel Detka <pawel.detka@monogo.pl>
2026-04-07 12:22:10 +0200

    Test

--- Model/Entity/Data/AttributeData.php
+++ Model/Entity/Data/AttributeData.php
@@ -68,6 +68,7 @@ class AttributeData
             $value = $product->getData($attributeName);
             $type = $product->getTypeId();
             $isComposite = in_array($type, ['configurable', 'grouped', 'bundle'], true);
+            $isArrayType = str_contains($attribute['type'], '[]');
 
             if ($value !== null) {
                 $productDataTmp = $this->addNonNullValue(
@@ -77,7 +78,7 @@ class AttributeData
                     $attribute,
                     $attributeResource
                 );
-                if (!$isComposite && !in_array($attributeName, $this->attributesToIndexAsArray, true)) {
+                if (!$isComposite || (!$isArrayType && !in_array($attributeName, $this->attributesToIndexAsArray, true))) {
                     continue;
                 }
             }
